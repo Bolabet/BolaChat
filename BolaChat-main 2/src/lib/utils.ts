1bcbb89a4dd91993adedf4399d4796ae8a0b79b5
Bolabet <unati@bolabet.co.zm>
2026-09-23 10:48:12 +0200

    Delete BolaChat-main 2 directory

--- BolaChat-main 2/src/lib/utils.ts
+++
@@ -1,4 +0,0 @@
-/** Joins class names, dropping falsy values. Keeps components free of clsx/cn dependencies. */
-export function cx(...classes: Array<string | false | null | undefined>): string {
-  return classes.filter(Boolean).join(" ");
-}
